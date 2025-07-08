dfaf55c57ea03b1f6abdc4e9f2a22d488ab34203
jennyo <jennifer.oleary.10@gmail.com>
2025-07-08 17:01:43 -0500

    student controller

--- src/main/java/com/example/class_manager/controllers/StudentController.java
+++ src/main/java/com/example/class_manager/controllers/StudentController.java
@@ -20,9 +20,22 @@ public class StudentController {
         return studentRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Student getById(@PathVariable Long id) { return studentRepository.findById(id).orElse(null); }
+
     @PostMapping
     public Student createStudent(@RequestBody Student student) {
         return studentRepository.save(student);
     }
+
+    @PutMapping("/{id}")
+    public Student update(@PathVariable Long id, @RequestBody Student student) {
+        String studentName = student.getName();
+        student.setName(studentName);
+        return studentRepository.save(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { studentRepository.deleteById(id); }
 }
 
