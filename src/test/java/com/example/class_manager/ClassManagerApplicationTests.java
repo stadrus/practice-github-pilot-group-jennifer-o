966c7f9a5757cc2117ebdd2a4bb1eeebcffac943
jennyo <jennifer.oleary.10@gmail.com>
2025-07-08 14:05:08 -0500

    init commit

---
+++ src/test/java/com/example/class_manager/ClassManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.class_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
