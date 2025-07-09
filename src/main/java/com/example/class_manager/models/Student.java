39246372fc218f73dec4f53a1d9ea872adc23637
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-07-09 09:55:02 -0500

    db update

--- src/main/java/com/example/class_manager/models/Student.java
+++ src/main/java/com/example/class_manager/models/Student.java
@@ -18,8 +18,12 @@ public class Student {
     }
 
     // Getters and setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public int getId() {
+        return id; }
+    public void setId(int id) {
+        this.id = id; }
+    public String getName() {
+        return name; }
+    public void setName(String name) {
+        this.name = name; }
 }
